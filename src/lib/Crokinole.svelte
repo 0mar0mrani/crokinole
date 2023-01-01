6b01ed2f992459655ef3144ae1250cf4b2395726
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-01-01 23:00:34 +0100

    Made a function to add players to the game

--- src/lib/Crokinole.svelte
+++ src/lib/Crokinole.svelte
@@ -1,4 +1,18 @@
-<script></script>
+	const players = [];
+	let currentPlayer = 0;
+	let scoreInput = 0;
+
+	function addNewPlayer(nameInput) {
+		const player = {
+			name: nameInput,
+			score: 0
+		}
+
+		players.push(player)
+	}
+
+	addNewPlayer('Omar');
+	addNewPlayer('Amalie');
 
 <section class="crokinole">
 	<h1>Crokinole</h1>
