cc12c3cb60bc8413af1a84267a073660da4d793f
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-01-01 20:51:47 +0100

    Initial project set-up

---
+++ src/lib/Crokinole.svelte
@@ -0,0 +1,11 @@
+<script></script>
+
+<section class="crokinole">
+	<h1>Crokinole</h1>
+</section>
+
+<style>
+	.crokinole {
+		background-color: rebeccapurple;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Crokinole from '../lib/Crokinole.svelte';
+</script>
+
+<div class="container">	
+	<Crokinole/>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100dvw;
+		height: 100dvh;
+		background-color: rebeccapurple;
+	}
+</style>
