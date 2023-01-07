5f62a3bbe960a7581af5caf3046ef14c9da50839
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-01-07 15:48:55 +0100

    Styled players component

--- src/lib/crokinole-components/CrokinolePlayers.svelte
+++ src/lib/crokinole-components/CrokinolePlayers.svelte
@@ -1,4 +1,6 @@
 <script>
+	import CloseSVG from "./CloseSVG.svelte";
+
 	export let players;
 	export let visibility;
 	export let handleAddPlayerClick;
@@ -9,38 +11,40 @@
 </script>
 
 <div class={`crokinole__players ${visibility ? 'crokinole__players--open' : ''}`}>
-	<div>Add players</div>
+	<div class="crokinole__header">Change Players</div>
 
 	<div class="crokinole__player-container">
 		{#each players as player, index}
 			<div class="crokinole__player" data-id={index}>
 				<div>
-					{`P${index + 1}`}
-				</div>
-
-				<div>
 					{player.name}
 				</div>
 
 				<button
+					class="crokinole__player-delete-button"
 					on:click={handleDeleteClick}>
-					Delete
+					<CloseSVG/>
 				</button>
 			</div>
 		{/each}
 	</div>
 
-	<div>
-		<input type="text" bind:value={nameInputChild} >
+	<div class="crokinole__input">
+		<input 
+			type="text" 
+			class="crokinole__input-text"
+			bind:value={nameInputChild}
+		>
 
 		<button
+			class={`crokinole__button ${nameInputChild !== '' ? '' : 'crokinole__button--deactivated'}`}
 			on:click={handleAddPlayerClick}
 			>Add player
 		</button>
 	</div>
 
 	<button
-		class={!isEnoughPlayers ? 'crokinole__button--deactivated' : ''}
+		class={`crokinole__button ${!isEnoughPlayers ? 'crokinole__button--deactivated' : ''}`}
 		on:click={handleStartGameClick}
 		>Start Game
 	</button>
@@ -57,7 +61,8 @@
 		left: 0;
 		height: 100%;
 		width: 100%;
-		background-color: red;
+		background-color: #a3622c78;
+		backdrop-filter: blur(10px);
 		z-index: 200;
 	}
 
@@ -65,8 +70,45 @@
 		display: flex;
 	}
 
+	.crokinole__header {
+		font-size: 4rem;
+		font-weight: 500;
+		margin-bottom: 2rem;
+		border-bottom: solid 2px #000;
+	}
+
+	.crokinole__player-container {
+		height: 16rem;
+	}
+
 	.crokinole__player {
 		display: flex;
+		gap: 4rem;
+		justify-content: space-between;
+	}
+
+	.crokinole__player-delete-button {
+		width: 4rem;
+		height: 4rem;
+		border: none;
+	}
+
+	.crokinole__input {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.crokinole__input-text {
+		border: solid 2px #000;
+		padding-left: 0.5rem;
+		text-align: center;
+	}
+	
+	.crokinole__button {
+		padding: 1rem 2rem;
+		border: solid 2px #000;
+		border-radius: 2rem;
+		margin-top: 2rem;
 	}
 
 	.crokinole__button--deactivated {
