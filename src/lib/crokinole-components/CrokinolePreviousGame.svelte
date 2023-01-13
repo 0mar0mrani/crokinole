40236dc16498c77b47521d90340fd9c91acd41fd
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-01-13 17:12:31 +0100

    Created a functional previous games

---
+++ src/lib/crokinole-components/CrokinolePreviousGame.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	type Player = {
+		name: string,
+		totalScore: number,
+		currentScore: number,
+		id: string,
+		isPlaying : boolean,
+	}
+	
+	type Game = {
+		time: string
+		date: string;
+		rounds: number;
+		score: Player[];
+	}
+
+	export let previousGame: Game;
+	let isGameOpen = false;
+
+	function handlePreviousClick() {
+		isGameOpen = !isGameOpen;
+	}
+</script>
+
+<div class="crokinole__previous-game">
+	<button 
+		on:click={handlePreviousClick}
+		class="crokinole__button">
+
+		<div>
+			{previousGame.score[0].name}
+		</div>
+
+		<div class="crokinole__header-score">
+			{previousGame.score[0].totalScore}p
+		</div>
+
+		<div class="crokinole__date">
+			{previousGame.date}
+		</div>
+	</button>
+
+	{#if isGameOpen}
+		<div class="crokinole__more-info">
+			<div class="crokinole__rounds-time-container">
+				<div class="crokinole__rounds">
+					Rounds {previousGame.rounds}	
+				</div>
+				
+				<div class="crokinole__time">
+					{previousGame.time}
+				</div>
+			</div>
+
+			{#each previousGame.score as player, index}
+				<div class="crokinole__player">
+					<div class="crokinole__place"># 
+						{index + 1}
+					</div>
+
+					<div class="crokinole__name">
+						{player.name}
+					</div>
+
+					<div class="crokinole__score">
+						{player.totalScore}
+					</div>	
+				</div>
+			{/each}
+		</div>
+	{/if}
+</div>
---
+++ src/lib/crokinole-components/CrokinolePreviousGames.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import CrokinolePreviousGame from "./CrokinolePreviousGame.svelte";
+
+	type Player = {
+		name: string,
+		totalScore: number,
+		currentScore: number,
+		id: string,
+		isPlaying : boolean,
+	}	
+	
+	type Game = {
+		time: string
+		date: string;
+		rounds: number;
+		score: Player[];
+	}
+
+	export let isPreviousGamesOpen: boolean;
+	export let previousGames: Game[];
+	export let handleBackClick: void;
+</script>
+
+{#if isPreviousGamesOpen}
+	<div class="crokinole__previous-games"> 
+		<div class="crokinole__header">Previous Games</div>
+
+		<div class="crokinole__previous-games-container">
+			{#each previousGames as previousGame}
+				<CrokinolePreviousGame
+					{previousGame}
+				/>
+			{/each}
+		</div>
+
+		<button class="crokinole__button" on:click={handleBackClick}>Back</button>
+	</div>
+{/if}
+
